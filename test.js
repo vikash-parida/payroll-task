945bcf9ee4478da2fcabe78cf1face01151612c2
vikash-parida <vikashparida067@gmail.com>
2022-09-28 13:01:26 +0530

    check falsy value code

--- test.js
+++ test.js
@@ -7,3 +7,12 @@
 
 // let result1 = _.difference([5,10],[1,2,3,4,5])
 // console.log(result1);
+
+//                           check falsy value or not
+// const myVar = "   ";
+
+// if (!myVar) {
+//   console.log('myVar is falsy');
+// } else {
+//   console.log('myVar is truthy');
+// }
